3e725c37cf1765a19685b388022568168dc7f40f
Dustin Pham <dpham008@ucr.edu>
2014-11-03 22:46:49 -0800

    updated cp.cpp to deal with directories and existing files/folders

--- src/cp.cpp
+++ src/cp.cpp
@@ -25,7 +25,17 @@ void readwrite(char in[], char out[]) {
     char buf[1];
 
     int fd = open(in, O_RDONLY);
-    int fdd = open(out, O_RDWR);
+    if (fd == -1) {
+        perror("Error with open");
+        exit(1);
+    }
+
+    int fdd = open(out, O_WRONLY);
+    if (fdd == -1) {
+        perror("Error with open");
+        exit(1);
+    }
+
     int red;
     while (red = read(fd, &buf, 1) != '\0') {
         if (red == -1) {
@@ -39,24 +49,97 @@ void readwrite(char in[], char out[]) {
     }
 }
 void readwritebuf(char in[], char out[]) {
-    char buf[BUFSIZ];
+    char* buf = new char[BUFSIZ];
 
     int fd = open(in, O_RDONLY);
-    int fdd = open(out, O_RDWR);
+    if (fd == -1) {
+        perror("Error with open");
+        exit(1);
+    }
 
-    if (read(fd, &buf, BUFSIZ) == -1) {
+    int fdd = open(out, O_WRONLY);
+    if (fdd == -1) {
+        perror("Error with open");
+        exit(1);
+    }
+
+    if (read(fd, buf, BUFSIZ) == -1) {
         perror("Error with read");
         exit(1);
     }
-    if (write(fdd, &buf, BUFSIZ) == -1) {
+    if (write(fdd, buf, BUFSIZ) == -1) {
         perror("Error with write");
         exit(1);
     }
+
+    delete [] buf;
 }
 
 int main(int argc, char* argv[]) {
     string first = argv[1];
     string second = argv[2];
+    string temp;
+    struct stat s;
+
+    if (first.at(0) != '.' && first.at(1) != '/') {
+        temp = "./";
+        first = temp + first;
+
+        if (stat(first.c_str(), &s) == -1) {
+            perror("Error with stat");
+            exit(1);
+        }
+        if (s.st_mode & S_IFDIR) {
+            cout << "First argument is a directory" << endl;
+            exit(1);
+        }
+    }
+    else {
+        if (stat(argv[1], &s) == -1) {
+            perror("Error with stat");
+            exit(1);
+        }
+        if(s.st_mode & S_IFDIR) {
+            cout << "First argument is a directory" << endl;
+        }
+    }
+
+    if (ifstream(argv[2])) {
+        cout << "Error: Second argument already exists as a file or folder." << endl;
+        exit(1);
+    }
+    else {
+        ofstream make(argv[2]);
+        if (!make.good()) {
+            cout << "Cannot open file" << endl;
+            exit(1);
+        }
+        make.close();
+    }
+
+    if (second.at(0) != '.' && second.at(1) != '/') {
+        temp = "./";
+        second = temp + second;
+
+        if (stat(second.c_str(), &s) == -1) {
+            perror("Error with stat");
+            exit(1);
+        }
+        if (s.st_mode & S_IFDIR) {
+            cout << "Second argument is a directory" << endl;
+            exit(1);
+        }
+    }
+    else {
+        if (stat(argv[2], &s) == -1) {
+            perror("Error with stat");
+            exit(1);
+        }
+        if (s.st_mode & S_IFDIR) {
+            cout << "Second argument is a directory" << endl;
+            exit(1);
+        }
+    }
 
     if (argc > 4 || argc < 3) {
         cout << "Error: Need 2 or 3 arguments." << endl;
